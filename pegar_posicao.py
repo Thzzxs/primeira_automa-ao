726c61b8c3a533b739c45defbda926e26896655f
Thiago Marinho <142832960+Thzzxs@users.noreply.github.com>
2024-07-11 10:54:09 -0300

    arquivos

---
+++ pegar_posicao.py
@@ -0,0 +1,7 @@
+import time
+import pyautogui  # type: ignore
+
+time.sleep(5)
+print(pyautogui.position())
+
+pyautogui.scroll(200)
---
+++ texto_instalaço.py
@@ -0,0 +1,7 @@
+#abrir o terminal
+#ir na setinha do lado do +
+#abrir comand prompt
+#colocar esses dois códiogos abaixo >>>>>
+
+#pip install pyautogui
+#pip install pandas openpyxl numpy
